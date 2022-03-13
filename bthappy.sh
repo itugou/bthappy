d694150b014c6a30f2c8d4b2ca6ddd40a25ee55d
ztkink <80385221+ztkink@users.noreply.github.com>
2022-03-13 14:38:36 +0800

    Update bthappy.sh

--- bthappy.sh
+++ bthappy.sh
@@ -1,5 +1,7 @@
 sed -i "s|if (bind_user == 'True') {|if (bind_user == 'REMOVED') {|g" /www/server/panel/BTPanel/static/js/index.js
 rm -rf /www/server/panel/data/bind.pl
+rm -rf /www/server/panel/data/plugin.json
+rm -rf /www/server/panel/data/repair.json
 cd /www/server/panel/data
 wget https://cdn.jsdelivr.net/gh/ztkink/bthappy@latest/plugin.json
 wget https://cdn.jsdelivr.net/gh/ztkink/bthappy@latest/repair.json

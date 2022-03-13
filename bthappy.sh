bc79cbc82552c22fec10854ed7689337fadfc734
ztkink <80385221+ztkink@users.noreply.github.com>
2022-03-13 14:18:48 +0800

    Update bthappy.sh

--- bthappy.sh
+++ bthappy.sh
@@ -1,3 +1,5 @@
+sed -i "s|if (bind_user == 'True') {|if (bind_user == 'REMOVED') {|g" /www/server/panel/BTPanel/static/js/index.js
+rm -rf /www/server/panel/data/bind.pl
 cd /www/server/panel/data
 wget https://cdn.jsdelivr.net/gh/ztkink/bthappy@latest/plugin.json
 chattr +i /www/server/panel/data/plugin.json

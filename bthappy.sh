09c153cc00ccb4ec584c37bc42ee82d62379604a
ztkink <80385221+ztkink@users.noreply.github.com>
2022-03-13 15:35:25 +0800

    Update bthappy.sh

--- bthappy.sh
+++ bthappy.sh
@@ -1,16 +1,47 @@
+#!/bin/bash
+LANG=en_US.UTF-8
+
+#修改强制登录开始
 sed -i "s|if (bind_user == 'True') {|if (bind_user == 'REMOVED') {|g" /www/server/panel/BTPanel/static/js/index.js
 rm -rf /www/server/panel/data/bind.pl
-echo "去除强制登录结束"
-sleep 5s
-echo "5秒后开始开心"
-chattr -i /www/server/panel/data/plugin.json
-chattr -i /www/server/panel/data/repair.json
-rm -rf /www/server/panel/data/plugin.json
-rm -rf /www/server/panel/data/repair.json
-cd /www/server/panel/data
-wget https://cdn.jsdelivr.net/gh/ztkink/bthappy@latest/plugin.json
-wget https://cdn.jsdelivr.net/gh/ztkink/bthappy@latest/repair.json
-sleep 5s
-echo "5秒后开始修改plugin和repair权限，禁止更改"
-chattr +i /www/server/panel/data/plugin.json
-chattr +i /www/server/panel/data/repair.json
+#修改强制登录结束
+echo -e "修改强制中..."
+sleep 2
+echo -e "修改强制登陆结束."
+
+#判断plugin.json文件是否存在,存在删除之后再下载,不存在直接下载
+plugin_file="/www/server/panel/data/plugin.json"
+if [ -f ${plugin_file} ];then
+    chattr -i /www/server/panel/data/plugin.json
+    rm /www/server/panel/data/plugin.json
+    cd /www/server/panel/data
+    wget https://cdn.jsdelivr.net/gh/ztkink/bthappy@latest/plugin.json
+    chattr +i /www/server/panel/data/plugin.json
+else
+    cd /www/server/panel/data
+    wget https://cdn.jsdelivr.net/gh/ztkink/bthappy@latest/plugin.json
+    chattr +i /www/server/panel/data/plugin.json
+fi
+echo -e "插件开心结束."
+sleep 3
+
+#判断repair.json文件是否存在,存在删除之后再下载,不存在直接下载
+repair_file="/www/server/panel/data/repair.json"
+if [ -f ${repair_file} ];then
+    chattr -i /www/server/panel/data/repair.json
+    rm /www/server/panel/data/repair.json
+    cd /www/server/panel/data
+    wget https://cdn.jsdelivr.net/gh/ztkink/bthappy@latest/repair.json
+    chattr +i /www/server/panel/data/repair.json
+else
+    cd /www/server/panel/data
+    wget https://cdn.jsdelivr.net/gh/ztkink/bthappy@latest/repair.json
+    chattr +i /www/server/panel/data/repair.json
+fi
+echo -e "防修改结束."
+sleep 3
+     /etc/init.d/bt restart 	
+sleep 3
+    bt default
+sleep 2 
+echo -e "开心结束！"
